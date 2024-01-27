16b629b837abd3da4543ac9b755be3b52b93c26c
Rodrigo Schneider <55472256+rodrigo-schneider@users.noreply.github.com>
2024-01-27 08:32:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ let btn = document.getElementById("button");
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    const mensagem = document.createElement("p")
+
     if (inputB.value > inputA.value) {
-        alert("Formulário válido");
+        mensagem.innerHTML = "Formulário válido"
+        document.getElementById("form").appendChild(mensagem)
     } else {
-        alert("Formulário inválido");
+        mensagem.innerHTML = "Formulário inválido"
+        document.getElementById("form").appendChild(mensagem)
     }
 });
