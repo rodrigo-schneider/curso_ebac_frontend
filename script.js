868ed477b40ebfb24cf31d65841df818c873d3f0
Rodrigo Schneider <55472256+rodrigo-schneider@users.noreply.github.com>
2024-03-28 11:24:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-
+// Classe abstrata
+class Animal {
+    constructor(name) {
+      if (this.constructor === Animal) {
+        throw new Error("Cannot instantiate abstract class");
+      }
+      this.name = name;
+    }
+  
+    // Método abstrato
+    makeSound() {
+      throw new Error("Abstract method not implemented");
+    }
+  }
+  
+  // Classes que herdam de Animal
+  class Dog extends Animal {
+    constructor(name) {
+      super(name);
+    }
+  
+    makeSound() {
+      return `${this.name} says Woof!`;
+    }
+  }
+  
+  class Cat extends Animal {
+    constructor(name) {
+      super(name);
+    }
+  
+    makeSound() {
+      return `${this.name} says Meow!`;
+    }
+  }
+  
+  // Criando instâncias de objetos
+  const dog1 = new Dog("Buddy");
+  const cat1 = new Cat("Whiskers");
+  const dog2 = new Dog("Max");
+  
+  console.log(dog1.makeSound()); // Saída: Buddy says Woof!
+  console.log(cat1.makeSound()); // Saída: Whiskers says Meow!
+  console.log(dog2.makeSound()); // Saída: Max says Woof!
+  
